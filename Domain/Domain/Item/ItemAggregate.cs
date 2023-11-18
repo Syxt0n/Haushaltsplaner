24ab74bb4d4b5ce8ef42baa331c3eea1f5734769
Leon Federlein <leonfederlein@gmail.com>
2023-11-18 14:48:11 +0100

    Domain-Model

---
+++ Domain/Domain/Item/ItemAggregate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Shared;
+
+
+namespace Domain.Item;
+public class ItemAggregate : AggregateRoot
+{
+	public string Name { get; private set; }
+	public bool Deleted { get; private set; }
+}
