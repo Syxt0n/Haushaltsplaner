b932f2995cde06233abda2e199fe6c6df11c3dc4
Syxt0n <leonfederlein@gmail.com>
2023-12-29 01:56:25 +0100

    Configurations for food aggregate

--- Application/EFCore/AppContext.cs
+++ Application/EFCore/AppContext.cs
@@ -13,20 +13,93 @@ namespace Application.EFCore;
 
 public class AppContext: DbContext
 {
-    public DbSet<Food> Foods {get;set;}
-    public DbSet<Shoppinglist> Shoppinglists {get;set;}
-    public DbSet<Mealtype> Mealtypes {get;set;}
-    public DbSet<Mealplan> Mealplans {get;set;}
-    public DbSet<Choreplan> Choreplans {get;set;}
-    public DbSet<Calendar> Calendars {get;set;}
-    public DbSet<Person> Persons {get;set;}
-    */
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<Food> Foods { get; set; }
     private string DbPath = "";
 
+    
     public AppContext()
     {
         DbPath = "Server=federlein.website;Database=hpEfCore;Trusted_Connection=True;";
     }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseNpgsql($"Data Source={DbPath}");
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
+
+
+#region Configs
+// Configuration for Item
+public class ItemConfiguration : IEntityTypeConfiguration<Item>
+{
+    public void Configure(EntityTypeBuilder<Item> builder)
+    {
+        builder.ToTable("items", "main");
+
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.Name).HasColumnName("name").IsRequired();
+
+        builder.Property(i => i.Deleted).HasColumnName("deleted");
+    }
+}
+
+// Configuration for Ingredient
+public class IngredientConfiguration : IEntityTypeConfiguration<Ingredient>
+{
+    public void Configure(EntityTypeBuilder<Ingredient> builder)
+    {
+        builder.ToTable("ingredients", "main");
+
+        builder.HasKey(i => new { i.FoodId, i.ItemId });
+
+        builder.Property(i => i.Amount).HasColumnName("amount").IsRequired();
+
+        builder.HasOne(i => i.Food)
+            .WithMany(f => f.Ingredients)
+            .HasForeignKey(i => i.FoodId)
+            .IsRequired();
+
+        builder.OwnsOne(i => i.Item, item =>
+        {
+            item.Property(p => p.Name).HasColumnName("name").IsRequired();
+        });
+    }
+}
+
+// Configuration for Food
+public class FoodConfiguration : IEntityTypeConfiguration<Food>
+{
+    public void Configure(EntityTypeBuilder<Food> builder)
+    {
+        builder.ToTable("food", "main");
+
+        builder.HasKey(f => f.Id);
+
+        builder.Property(f => f.Name).HasColumnName("name").IsRequired();
+
+        builder.Property(f => f.Deleted).HasColumnName("deleted");
+
+        builder.OwnsMany(f => f.Ingredients, ingr =>
+        {
+            ingr.ToTable("ingredients", "main");
+            ingr.HasKey(i => new { i.FoodId, i.ItemId });
+
+            ingr.Property(i => i.Amount).HasColumnName("amount").IsRequired();
+
+            ingr.OwnsOne(i => i.Item, item =>
+            {
+                item.Property(p => p.Name).HasColumnName("name").IsRequired();
+            });
+        });
+    }
+}
+
+#endregion
